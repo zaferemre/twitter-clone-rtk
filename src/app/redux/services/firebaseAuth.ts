dbb28face3af766ee5d5a8d0dbda1a1146393db4
zaferemre <emrecalisir34@gmail.com>
2025-04-24 20:30:54 +0300

    envlocal

--- src/app/redux/services/firebaseAuth.ts
+++ src/app/redux/services/firebaseAuth.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAdUq22m9ZUmRv2WX5hX5PS0sKMCvFdQfk",
-  authDomain: "twitter-clone-rtk.firebaseapp.com",
-  projectId: "twitter-clone-rtk",
-  storageBucket: "twitter-clone-rtk.firebasestorage.app",
-  messagingSenderId: "137201519200",
-  appId: "1:137201519200:web:d94ec31fce196f684c1105",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
